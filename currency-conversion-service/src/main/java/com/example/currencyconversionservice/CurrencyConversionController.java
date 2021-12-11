7cf2535b0ebe350fd503ee17eeb9872cbb69c39c
sapta <sbasu2324>
2021-12-11 12:35:59 +0530

    Adding feign rest client

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
@@ -3,15 +3,35 @@ package com.example.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping(path = "/currency-conversion")
 public class CurrencyConversionController {
-	@GetMapping("currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	
+	@Autowired
+	CurrencyExchangeProxy proxy;
+	
+	@GetMapping("/feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from,
+			@PathVariable String to, @PathVariable BigDecimal quantity) {
+		HashMap<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+		return new CurrencyConversion(10001L, from, to, 
+				quantity, currencyConversion.getConversionMultiple(),
+				quantity.multiply(currencyConversion.getConversionMultiple()), 
+				currencyConversion.getEnvironment());
+	}
+	
+	@GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
 			@PathVariable String to, @PathVariable BigDecimal quantity) {
 		HashMap<String, String> uriVariables = new HashMap<>();
