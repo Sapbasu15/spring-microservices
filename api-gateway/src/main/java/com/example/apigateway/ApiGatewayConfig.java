72c8e990ce5d486a0d98eaaba16ea870cac798d6
sapta <sbasu2324>
2021-12-11 22:15:59 +0530

    Added API gateway and URL routing

---
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayConfig.java
@@ -0,0 +1,29 @@
+package com.example.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.path("/get")
+						.filters(f -> f.addRequestHeader("x-header", "x-header-value")
+										.addRequestParameter("x-param", "x-param-value"))
+						.uri("http://httpbin.org:80"))
+				.route(p -> p.path("/currency-exchange/**")
+						.uri("lb://currency-exchange"))
+				.route(p -> p.path("/currency-conversion/**")
+						.uri("lb://currency-conversion"))
+				.route(p -> p.path("/currency-conversion-new/**")
+							.filters(f -> f.rewritePath(
+									"/currency-conversion-new/(?<segment>.*)", 
+									"/currency-conversion/${segment}"))
+						.uri("lb://currency-conversion"))
+				.build();
+	}
+}
